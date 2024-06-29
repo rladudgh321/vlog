a97c1a02c28d84ff3111a0900052f5e44d15ac5c
청양소방서 청양센터 <kyh_0625@naver.com>
2024-06-29 17:45:26 +0900

    page navagate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
+import Link from "next/link";
+import Header from "./common/Header";
+import Nav from "./common/Nav";
+
 export default function Home() {
   return (
     <div>
-      <div className="text-center p-4 text-3xl text-gray-500">WEB</div>
-      <hr />
-      <nav className="ml-4 p-4">
-        <ol>
-          <li>html</li>
-          <li>css</li>
-          <li>js</li>
-        </ol>
-      </nav>
-      <hr />
-      <br />
+      <Header />
+      <Nav />
       <h2 className="ml-4 text-2xl">WEB</h2>
       <br />
       <p className="ml-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam consequuntur eius architecto ipsum similique ut vero nam cupiditate, alias, quidem aperiam, id perspiciatis fugit soluta et saepe illo odit dolorum.</p>
